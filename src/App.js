710920e2f958d3c5f807bfa0ad7567d63ff646a1
bluebird <ij3512@naver.com>
2020-01-28 23:06:53 +0900

    연습 : react 개발 공부 중 -jsx문법 공부

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React from 'react';
 
+function Food({name, url}) {
+  return (
+      <div>
+        <h1> I like {name} </h1>
+        <img src={url} />
+      </div>
+      )
+}
+
+const foodILike = ["kimchi", "ramen", "samgiopsal"]
+const defaultImageUrl = "profile/Kunal_Chaudhury/publication/221935879/figure/fig1/AS:305325572935680@1449806697160/Test-image-Checker-of-size-256-256-consisting-of-black-intensity-0-and-white.png"
+
+function renderFood(dish) {
+  console.log(dish)
+  return  <Food name={dish} url={defaultImageUrl}/>
+}
+
+
 function App() {
   return (
-    <div className="App">Hello!!</div>
+    <div>
+
+      {foodILike.map(renderFood)}
+      {/*{foodILike.map(dish=> (<Food name={dish} url={defaultImageUrl}/> ))}*/}
+      {/*<Food fav={"kimchi"} />*/}
+
+    </div>
   );
 }
 
