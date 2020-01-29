b3e499581eddf100f413ed7b23df08928955b070
bluebird <ij3512@naver.com>
2020-01-29 22:24:41 +0900

    연습 : react 기능 공부 중 -npm i prop-types (prop-types : 매개변수 자료형 확인)

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
 import React from 'react';
+import Proptypes from 'prop-types';
 
-function Food({name, url}) {
+function Food({name, url, rating}) {
   return (
       <div>
         <h1> I like {name} </h1>
+          <h4>{rating}/5.0</h4>
         <img src={url} />
       </div>
       )
 }
+Food.propTypes = {
+    name: Proptypes.string.isRequired,
+    url: Proptypes.string.isRequired,
+    rating: Proptypes.number.isRequired
+}
 
-const foodILike = ["kimchi", "ramen", "samgiopsal"]
+const foodILike = [{
+    id: 1,
+    name: "kimchi",
+    rating : 5
+}, {
+    id: 2,
+    name: "ramen",
+    rating : 5.3
+}, {
+    id: 3,
+    name: "samgiopsal",
+    rating : 4.7
+}]
 const defaultImageUrl = "profile/Kunal_Chaudhury/publication/221935879/figure/fig1/AS:305325572935680@1449806697160/Test-image-Checker-of-size-256-256-consisting-of-black-intensity-0-and-white.png"
 
 function renderFood(dish) {
   console.log(dish)
-  return  <Food name={dish} url={defaultImageUrl}/>
+  return  <Food key={dish.id} name={dish.name} url={defaultImageUrl} rating={dish.rating}/>
 }
 
 
