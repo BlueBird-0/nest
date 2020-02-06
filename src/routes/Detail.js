ef53285dfc1bb3eb9cea734e69262f493e964554
bluebird <ij3512@naver.com>
2020-02-06 21:59:38 +0900

    연습 : react 기능 공부 중 -link(state) 이용해 상태변경

---
+++ src/routes/Detail.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Detail extends React.Component{
+    componentDidMount() {
+        const { location } = this.props
+        console.log(location.state)
+    }
+    render() {
+        return (
+            <div>Hello</div>
+        )
+    }
+}
+export default Detail
