0b60e7256acf306d052305ec22d59bf0279c25d5
bluebird <ij3512@naver.com>
2020-02-20 23:52:53 +0900

    연습 : react 홈페이지 개발 -Home 배경화면 이미지 및 투명도 변경 -! [bug] 이미지 불러오기 스크롤시 사이즈 변경으로 인한 잔랙 발생

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -11,10 +11,15 @@ class Home extends React.Component{
         return (
             <React.Fragment>
                 <div className={"home"}>
+                    <img src={"/images/home_background.jpg"}/>
+                    <div className={"opacity"}/>
                     <div>
                         <h1>전인학</h1>
                         <h1>https://github.com/BlueBird-0</h1>
                     </div>
+                    {/*<div>*/}
+                    {/*    <h1>전인학</h1>*/}
+                    {/*</div>*/}
                 </div>
             </React.Fragment>
             )
