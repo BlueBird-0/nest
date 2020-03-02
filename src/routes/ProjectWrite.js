5acc1f55c542161665c4fe36cf25f6baee0a208b
bluebird <ij3512@naver.com>
2020-03-02 23:50:23 +0900

    수정 : react 홈페이지 개발 -ProjectWriter 글쓰기 형식 수정

--- src/routes/ProjectWrite.js
+++ src/routes/ProjectWrite.js
@@ -1,14 +1,22 @@
 import React from 'react'
 
 function ProjectWrite() {
+    // var handleSubmit = (e) => {
+    //     e.preventDefault()
+    //     this.props.onSaveData()
+    //     this.setState({})
+    // }
+
     return (
         <div>
-            <form onSubmit={this.handleSubmit}>
-                <input placeholder="brdtitle" name="brdtitle" onChange={this.handleChange}/>
-                <input placeholder="brdgithub" name="brdgithub" onChange={this.handleChange}/>
-                <input placeholder="brbdcomment" name="brbdcomment" onChange={this.handleChange}/>
+            {/*<form onSubmit={this.handleSubmit}>*/}
+            <form>
+                <input placeholder="brdtitle" name="brdtitle" />
+                <input placeholder="brdgithub" name="brdgithub" />
+                <input placeholder="brbdcomment" name="brbdcomment" />
                 <button type="submit">Write</button>
             </form>
         </div>
     )
 }
+export default ProjectWrite
--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {makeStyles} from '@material-ui/core/styles'
 import Project01 from "./Project01";
+import ProjectWrite from "./ProjectWrite"
 import {Container, Grid, Paper, Typography} from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
@@ -24,7 +25,6 @@ function Projects() {
             </Grid>
         </Container>
         )
-
 }
 
 export default Projects
