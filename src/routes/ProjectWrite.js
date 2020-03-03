473753e70192c745dd9a3d57741cb3f1f8fea018
bluebird <ij3512@naver.com>
2020-03-03 23:58:57 +0900

    수정 : react 홈페이지 개발 -ProjectWriter 글쓰기 상태 저장 추가

--- src/routes/ProjectWrite.js
+++ src/routes/ProjectWrite.js
@@ -1,22 +1,35 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-function ProjectWrite() {
+class ProjectWrite extends Component {
     // var handleSubmit = (e) => {
     //     e.preventDefault()
     //     this.props.onSaveData()
     //     this.setState({})
     // }
+    state = {}
 
-    return (
-        <div>
-            {/*<form onSubmit={this.handleSubmit}>*/}
-            <form>
-                <input placeholder="brdtitle" name="brdtitle" />
-                <input placeholder="brdgithub" name="brdgithub" />
-                <input placeholder="brbdcomment" name="brbdcomment" />
-                <button type="submit">Write</button>
-            </form>
-        </div>
-    )
+    handleChange = (e) =>{
+        this.setState({
+            [e.target.name] : e.target.value()
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        console.log("wowowowwo")
+        this.setState({})
+    }
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input placeholder="brdtitle" name="brdtitle" />
+                    <input placeholder="brdgithub" name="brdgithub" />
+                    <input placeholder="brbdcomment" name="brbdcomment" />
+                    <button type="submit">Write</button>
+                </form>
+            </div>
+        )
+    }
 }
 export default ProjectWrite
